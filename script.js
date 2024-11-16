8bf2ade4b81ace6a5d897d5d782dd92b7f8a7b76
Rajudas575 <rajudas575@gmail.com>
2024-11-16 22:38:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById('ageForm').addEventListener('submit', function(event) {event.preventDefault();
+const age = document.getElementById('age').value;
+const name = document.getElementById('name').value;
+
+if (!age || !name) {
+	alert('Both fields are required!');
+	return;
+}
+
+const ageValidationPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) >= 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000); // 4 seconds delay
+  });
+
+ageValidationPromise
+    .then((name) => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch((name) => {
+      alert(`Oh sorry, ${name}. You aren't old enough.`);
+    });
+});
